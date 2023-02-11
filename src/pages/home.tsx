94d27964a5a57abb029b02d7f275e929e60e4894
Chris Moss <hcmoss70@gmail.com>
2023-02-11 18:06:08 -0500

    added search bar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,20 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Typography, TextField, Button, Stack } from "@mui/material"
+import Appbar from "../components/toolbar"
 
 const home = () => {
   return (
-    <Box>
-      <Typography>Hello there</Typography>
+    <Box sx={{ height: "100vh", width: "100vw", backgroundColor: "black" }}>
+      <Appbar />
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{ display: "flex", justifyContent: "center", mt: 20 }}
+      >
+        <TextField variant="outlined" sx={{ backgroundColor: "white" }} />
+        <Button variant="contained" sx={{ borderRadius: 50 }}>
+          Search
+        </Button>
+      </Stack>
     </Box>
   )
 }
