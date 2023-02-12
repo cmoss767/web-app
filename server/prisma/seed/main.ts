6eefea0d891583f7357c714f7f21b60440c65853
Chris Moss <hcmoss70@gmail.com>
2023-02-12 13:40:40 -0500

    seeded db

---
+++ server/prisma/seed/main.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client"
+import createUser from "./users"
+
+const prisma = new PrismaClient()
+
+const promiseArr: Promise<void>[] = []
+
+/** USERS */
+promiseArr.push(
+    createUser(prisma).then(
+        () => undefined
+      )
+)
+
+Promise.all(promiseArr).then(() => {
+    prisma.$disconnect()
+  })
---
+++ server/prisma/seed/users.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client"
+import bcrypt from "bcryptjs"
+
+const createUser = async (prisma: PrismaClient) => {
+  const salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync("chrisMoss123!", salt)
+  return await prisma.user.create({
+    data: {
+      first_name: "Chris",
+      last_name: "Moss",
+      phone: "+18439816390",
+      email: "hcmoss70@gmail.com",
+      password: hash,
+    },
+  })
+}
+
+export default createUser
