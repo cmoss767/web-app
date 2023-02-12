98cb506124d78699c61fe611eba70e52b4efa6bb
Chris Moss <hcmoss70@gmail.com>
2023-02-12 13:20:08 -0500

    set up db connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import "dotenv/config"
+import { PrismaClient } from "@prisma/client"
 
 const app = express()
+const prisma = new PrismaClient()
 
 app.listen(process.env.PORT, ()=>{
     console.info(`Server running on port ${process.env.PORT}`)
+    console.info(`Sucessfully connect to db ${process.env.DB_NAME}`)
 })
 
