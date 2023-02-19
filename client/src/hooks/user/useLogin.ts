92200140f2f74f87bbf233796ad9dec10d6e21a5
Chris Moss <hcmoss70@gmail.com>
2023-02-18 19:37:35 -0500

    removed local storage boolean

--- client/src/hooks/user/useLogin.ts
+++ client/src/hooks/user/useLogin.ts
@@ -2,21 +2,12 @@ import { useMutation } from "react-query"
 import axios from "axios"
 import { useLocalStorage } from "react-use"
 
-// const [authenticated, setAuthenticated, removeAuthenticated] =
-//   useLocalStorage<boolean>("authenticated", false, { raw: true })
-
 const login = (data: { email: string; password: string }) =>
   axios.post(`http://localhost:5000/users/auth`, data)
 
 const useLogin = () => {
-  return useMutation(
-    "login",
-    (data: { email: string; password: string }) => login(data),
-    {
-      // onSuccess: () => {
-      //   setAuthenticated(true)
-      // },
-    }
+  return useMutation("login", (data: { email: string; password: string }) =>
+    login(data)
   )
 }
 
